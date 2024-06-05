cfc8f1931e2e0392901bbe8db3b2bdc341bed0df
ForSev4705 <90548679+Haianh4705@users.noreply.github.com>
2024-06-05 13:51:29 +0000

    update

--- python/test.py
+++ python/test.py
@@ -1,11 +1,46 @@
 class Health:
+    def __init__(self, value: float):
+        self.max_hp: float
+        self.cur_hp: float
+    
+    def set_max_hp(self, value: float) -> bool:
+        if value < 1:
+            return False
+        
+        self.max_hp = value
+        if self.cur_hp > self.max_hp:
+            self.cur_hp = self.max_hp
+        return True
+    
+    def heal(self, value: float):
+        self.cur_hp += value
+        if self.cur_hp > self.max_hp:
+            self.cur_hp = self.max_hp
+
+class Normal:
+    def __init__(self):
+        self.name = "Normal Attack"
+        self.scale = 1
+    
+    def __str__(self) -> str:
+        return f"{self.name}: Perform a attack deal 100% ATK"
+
+class Slash:
     def __init__(self):
         pass
 
-class Attack:
+class Bat:
     def __init__(self):
         pass
 
 class Player:
     def __init__(self):
-        pass
+        self.hp = Health(100)
+        self.atk = 15
+        self.skills = [Normal()]
+    
+    def show_skill(self):
+        for id, skill in enumerate(self.skills):
+            print(id + 1, " | ", skill, sep='')
+
+Player().show_skill()
